f875e7b7d78424b44a8ce2151c2f22842edf5a9a
Pace Davis <pacemdavis@gmail.com>
2019-12-10 16:37:49 -0600

    ldldl

--- main.js
+++ main.js
@@ -120,3 +120,29 @@ const displayUsers = () => {
   })
 }
 
+
+
+function sockMerchant(n, ar) {
+  const colors = {};
+  let matches = 0;
+  for (let i = 0; i < n; i++) {
+      if (colors[ar[i]]) {
+          matches++;
+          colors[ar[i]] = 0;
+      } else {
+          colors[ar[i]] = 1;
+      }
+  }
+  return matches;
+}
+
+var ones = {}, pairs = 0;
+for (var i = 0; i < n; i++) {
+    if (ones.hasOwnProperty(ar[i])) {
+        pairs++;
+        delete ones[ar[i]];
+    } else {
+        ones[ar[i]] = 0;
+    }
+}
+return pairs
